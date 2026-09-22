be1e10b1b8f5a89a5fa5431e15b98d563bb6dc05
guilherme xavier <guimx1903@hotmail.com>
2026-09-22 20:36:39 -0300

    Atualização 22_09_2026

---
+++ script.js
@@ -0,0 +1,19 @@
+document.getElementById("year").textContent = new Date().getFullYear();
+
+function copyEmail() {
+  const email = "Guilherme.m.xavier@outlook.com.br";
+
+  navigator.clipboard
+    .writeText(email)
+    .then(() => {
+      const toast = document.getElementById("toast");
+      toast.classList.add("show");
+
+      setTimeout(() => {
+        toast.classList.remove("show");
+      }, 2500);
+    })
+    .catch(() => {
+      window.location.href = `mailto:${email}`;
+    });
+}
